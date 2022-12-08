5324399e956819b6d00ea1b0169544a5e7af04b9
Cesar Chavez <cesarplur@hotmail.com>
2022-12-08 15:29:42 -0600

    update 7

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ Route::get('/create', [AuditsController::class, 'create'])->name('Audits.create'
 Route::post('/edit/{1}', [AuditsController::class, 'edit'])->name('Audits.edit');
 Route::get('/show', [AuditsController::class, 'show'])->name('Audits.show');
 Route::get('/index', [AudittypeController::class, 'index'])->name('Audittype.index');
-Route::update('/edit/{1}', [AuditsController::class, 'edit'])->name('Audits.edit');
+Route::put('/update/{1}', [AuditsController::class, 'update'])->name('Audits.update');
